28ea6b448eb4801e2be4e89460e19c9858ecd7ca
Matthias Moises <mail@matthias-moises.de>
2025-03-16 22:14:12 +0100

    Updated results slots

--- src/components/main/ResultText.vue
+++ src/components/main/ResultText.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="text-4xl mb-3">
+  <div v-if="$slots.headline" class="text-4xl mb-3">
     <slot name="headline" />
   </div>
-  <div class="text-center my-5">
+  <div v-if="$slots.image" class="text-center my-5">
     <slot name="image" />
   </div>
-  <p class="text-2xl mb-3">
+  <p v-if="$slots.subtext1" class="text-2xl mb-3">
     <slot name="subtext1" />
   </p>
-  <div class="text-7xl mb-3"><slot name="score" /></div>
-  <p class="text-2xl mb-3">
+  <div v-if="$slots.score" class="text-7xl mb-3"><slot name="score" /></div>
+  <p v-if="$slots.subtext2" class="text-2xl mb-3">
     <slot name="subtext2" />
   </p>
 </template>
